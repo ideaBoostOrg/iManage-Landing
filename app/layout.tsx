45a85c12b58cb7435360816ce19e0cf04de88825
Prasad Lakshan <kpdplakshan@gmail.com>
2023-11-22 11:59:57 +0530

    chg: add some font chnages

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 
 export const metadata: Metadata = {
-  title: 'Travel',
-  description: 'Travel UI/UX App for Camping',
+  title: 'iManage ',
+  description: 'Optimize your cleaning business with iManage, the cutting-edge cleaning job management system. Streamline operations, enhance efficiency, and boost productivity with our powerful cleaning management software. From scheduling to task tracking, iManage is your all-in-one solution for seamless cleaning job coordination. Elevate your cleaning services with our user-friendly platform designed for unparalleled efficiency. Discover the future of cleaning management – choose iManage today for a spotless and organized workflow. Keywords: cleaning job management, cleaning management software, task tracking, scheduling, cleaning services, efficient workflow, job coordination, iManage.',
 }
 
 export default function RootLayout({
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -14,7 +14,7 @@ const Hero = () => {
           height={50}
           className="absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]"
         /> */}
-        <h1 className="bold-52 lg:bold-88">Revolutionize Your Cleaning Business with iManage!</h1>
+        <h1 className="bold-52 lg:bold-48">Revolutionize Your Cleaning Business with iManage!</h1>
         <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
           Elevate Your Cleaning Business with our all in one App and Portal
           Solution Effortlessly oversee your cleaning business, from job
